9d319cb3b39c0f2b512ee16259ec92a80ce5c157
yunjinqi <yunjinqi@qq.com>
2025-10-17 15:14:57 +0800

    perf(indicators): enable Cython optimizations for myind/momentum/mabase/lrsi/kst/kama/ichimoku/hurst.pyx

--- cybacktrader/indicators/myind.pyx
+++ cybacktrader/indicators/myind.pyx
@@ -1,6 +1,9 @@
 
 # Cython性能优化标记
 # cython: language_level=3
+# cython: boundscheck=False
+# cython: wraparound=False
+# cython: cdivision=True
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
