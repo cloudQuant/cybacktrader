22b0679e26f1ff03d06ea131e0e8d3dc93f0b79d
yunjinqi <yunjinqi@qq.com>
2025-10-18 11:37:36 +0800

    Deep optimize position.pyx: add directives, typed locals, cached vars

--- cybacktrader/position.pyx
+++ cybacktrader/position.pyx
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; py-indent-offset:4 -*-
 
-# Cython性能优化标记（保守设置）
+# Cython深度性能优化标记（完整版）
 # cython: language_level=3
+# cython: boundscheck=False
+# cython: wraparound=False
+# cython: cdivision=True
+# cython: nonecheck=False
+# cython: initializedcheck=False
 # cython: infer_types=True
+# cython: optimize.unpack_method_calls=True
+# cython: optimize.use_switch=True
 
 from copy import copy
 
@@ -62,41 +69,50 @@ class Position(object):
         # 如果现在的持仓大于0,并且理论上的size大于当前的size，就意味着要新开仓；
         # 如果理论上的size小于等于当前的持仓，那么开仓量是0和理论上size的最小量；
         # 平仓量等于当前持仓和当前持仓减去理论持仓量最小的一个值
-        if self.size > 0:
-            if size > self.size:
-                self.upopened = size - self.size  # new 10 - old 5 -> 5
-                self.upclosed = 0
+        cdef long cur_size = self.size
+        cdef long new_size = size
+        cdef long upopened, upclosed
+        cdef double price_d = price
+
+        if cur_size > 0:
+            if new_size > cur_size:
+                upopened = new_size - cur_size  # new 10 - old 5 -> 5
+                upclosed = 0
             else:
                 # same side min(0, 3) -> 0 / reversal min(0, -3) -> -3
-                self.upopened = min(0, size)
+                upopened = 0 if new_size > 0 else new_size
                 # same side min(10, 10 - 5) -> 5
                 # reversal min(10, 10 - -5) -> min(10, 15) -> 10
-                self.upclosed = min(self.size, self.size - size)
+                upclosed = cur_size if cur_size < (cur_size - new_size) else (cur_size - new_size)
         # 当前持仓小于0的时候，有类似的效果
-        elif self.size < 0:
-            if size < self.size:
-                self.upopened = size - self.size  # ex: -5 - -3 -> -2
-                self.upclosed = 0
+        elif cur_size < 0:
+            if new_size < cur_size:
+                upopened = new_size - cur_size  # ex: -5 - -3 -> -2
+                upclosed = 0
             else:
                 # same side max(0, -5) -> 0 / reversal max(0, 5) -> 5
-                self.upopened = max(0, size)
+                upopened = 0 if new_size < 0 else new_size
                 # same side max(-10, -10 - -5) -> max(-10, -5) -> -5
                 # reversal max(-10, -10 - 5) -> max(-10, -15) -> -10
-                self.upclosed = max(self.size, self.size - size)
+                upclosed = cur_size if cur_size > (cur_size - new_size) else (cur_size - new_size)
         # 如果当前持仓等于0，新开仓和平仓都等于0
         # todo 这里面直接使用0代替self.size可能可以提高效率
         else:  # self.size == 0
-            self.upopened = self.size
-            self.upclosed = 0
+            upopened = cur_size
+            upclosed = 0
+
         # 实际持仓大小
-        self.size = size
+        self.size = new_size
         # 原始价格
         self.price_orig = self.price
         # 如果持仓大小大于0的话，当前价格就等于price，否则当前价格等于0
-        if size:
-            self.price = price
+        if new_size:
+            self.price = price_d
         else:
             self.price = 0.0
+
+        self.upopened = upopened
+        self.upclosed = upclosed
 
         return self.size, self.price, self.upopened, self.upclosed
     # 调用len(position)的时候，返回持仓的绝对值
@@ -167,48 +183,55 @@ class Position(object):
         # 原始的价格
         self.price_orig = self.price
         # 旧地持仓大小
-        oldsize = self.size
+        cdef long oldsize = self.size
+        cdef long delta = size
+        cdef long opened, closed
+        cdef double price_d = price
+        cdef double newprice
+
         # 新地持仓大小
-        self.size += size
+        self.size = oldsize + delta
         # 如果size是0的话
         if not self.size:
             # Update closed existing position
             # 更新开仓、平仓和价格
-            opened, closed = 0, size
+            opened, closed = 0, delta
             self.price = 0.0
         # 如果position的持仓是0的话，需要开仓size的量
         elif not oldsize:
             # Update opened a position from 0
-            opened, closed = size, 0
-            self.price = price
+            opened, closed = delta, 0
+            self.price = price_d
         # 如果原先position的持仓是0的话
         elif oldsize > 0:  # existing "long" position updated
             # 如果增加的仓位大于0,那么就需要新开仓，并且计算平均持仓价格
-            if size > 0:  # increased position
-                opened, closed = size, 0
-                self.price = (self.price * oldsize + size * price) / self.size
+            if delta > 0:  # increased position
+                opened, closed = delta, 0
+                newprice = (self.price * oldsize + delta * price_d) / self.size
+                self.price = newprice
             # 如果平掉size之后，持仓仍然大于0,那么就平仓size
             elif self.size > 0:  # reduced position
-                opened, closed = 0, size
+                opened, closed = 0, delta
                 # self.price = self.price
             # 其他情况下，就需要开仓self.size,平仓-oldsize
             else:  # self.size < 0 # reversed position form plus to minus
                 opened, closed = self.size, -oldsize
-                self.price = price
+                self.price = price_d
         # 原有的持仓是负数
         else:  # oldsize < 0 - existing short position updated
             # 如果新增加的仓位也是负数，那么就新开size
-            if size < 0:  # increased position
-                opened, closed = size, 0
-                self.price = (self.price * oldsize + size * price) / self.size
+            if delta < 0:  # increased position
+                opened, closed = delta, 0
+                newprice = (self.price * oldsize + delta * price_d) / self.size
+                self.price = newprice
             # 如果当前self.size小于0,平仓size
             elif self.size < 0:  # reduced position
-                opened, closed = 0, size
+                opened, closed = 0, delta
                 # self.price = self.price
             # 其他情况下，就需要开仓self.size,平仓-oldsize
             else:  # self.size > 0 - reversed position from minus to plus
                 opened, closed = self.size, -oldsize
-                self.price = price
+                self.price = price_d
         # 开仓和平仓量
         self.upopened = opened
         self.upclosed = closed
